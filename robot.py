a93b4f128eb7542b7e7e7075d91fdf5a140c4c9b
Finlay Hicks <84518177+f-hicks@users.noreply.github.com>
2024-08-05 17:45:49 +0100

    Add files via upload

---
+++ robot.py
@@ -0,0 +1,30 @@
+from sbot import Robot
+from sbot import BRAKE, COAST
+rbot = Robot()
+
+fudgeFactor=0.99403
+
+def drive_straight(time, power):
+    rbot.motor_board.motors[0].power = power*fudgeFactor
+    rbot.motor_board.motors[1].power = power
+    rbot.sleep(10)
+    rbot.motor_board.motors[0].power = COAST
+    rbot.motor_board.motors[1].power = COAST
+
+def turn(degrees, direction, power):
+    if direction: 
+        rbot.motor_board.motors[0].power = power*fudgeFactor
+        rbot.motor_board.motors[1].power = -power
+    else:
+        rbot.motor_board.motors[0].power = -power*fudgeFactor
+        rbot.motor_board.motors[1].power = power
+    rbot.sleep(degrees*0.007)
+    rbot.motor_board.motors[0].power = COAST
+    rbot.motor_board.motors[1].power = COAST
+
+
+
+
+drive_straight(10,0.25)
+
+#turn(90,1,0.25)
